2891dfa4d052a1f47513799b617c80020b3539dc
Devin Picht <dpicht@Devins-MacBook-Air.local>
2019-12-10 20:35:59 -0600

    finished part 1

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -8,8 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class FavPhotosComponent implements OnInit {
   photosTitle = 'Photo/Image Section Title Here';
   image1 = 'https://www.launchcode.org/assets/icons/trophy-95e8cbe9bfda44123422302951deb1c92a237d39052669b8fbfafec00cb4f608.png';
-  image2 = '';
-  image3 = '';
+  image2 = 'https://images.pexels.com/photos/533923/pexels-photo-533923.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500';
+  image3 = 'https://res.cloudinary.com/twenty20/private_images/t_watermark-criss-cross-10/v1458510198000/photosp/3c06002c-d1b8-44d1-9559-5f1b816847f1/stock-photo-outdoors-lawn-summer-patio-backyard-spring-relaxing-peace-countryside-3c06002c-d1b8-44d1-9559-5f1b816847f1.jpg';
 
   constructor() { }
 
